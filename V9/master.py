64c426b8ebe34225d43aa6a7d962a0a01ff8b4e6
KhanhNguyen <ankhanh1566@gmail.com>
2021-11-08 21:54:29 +0700

    U

--- V9/master.py
+++ V9/master.py
@@ -1,5 +1,6 @@
 import main
+# TODO: pause Minecraft when not minecraftAPI.isFocused()
 
 # TODO: when have .rar in /download => extract it
-# TODO: focused to the window have mouseover  
+# TODO: focused to the window have mouseover
 # TODO: Hold Alt => scroll faster
